2bb2a6f11e5b8eb33c75aa326e7c45f6ea0429ee
Adadezer <adadezer@gmail.com>
2022-11-10 15:13:45 -0300

    feat(frontend): :sparkles: adição do header

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,7 +1,11 @@
 import React from "react"
+import Header from "../components/Header"
 
 export default function Tasks() {
   return (
-    <h1>tasks teste</h1>
+    <>
+      <Header titleHeader="Tarefas"/>
+      <h1>tasks teste</h1>
+    </>
   )
 }
