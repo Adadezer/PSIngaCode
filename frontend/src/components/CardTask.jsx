cbc885b937aa805e189fd59a3b13b521a5ce7b3c
Adadezer <adadezer@gmail.com>
2022-11-11 11:20:44 -0300

    feat(frontend): :sparkles: detalhes da tarefa

--- frontend/src/components/CardTask.jsx
+++ frontend/src/components/CardTask.jsx
@@ -1,12 +1,28 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
+import { Box, Grid, Modal } from "@mui/material";
 
 export default function BasicCard(props) {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #1b80f5",
+    boxShadow: 54,
+    p: 4,
+  };
+
+  const [open, setOpen] = useState(false);
+  const OpenModal = () => setOpen(true);
+  const CloseModal = () => setOpen(false);
+
   return (
     <Grid item {...props} >
       <Card sx={{ minWidth: 380, margin: 1 }}>
@@ -22,17 +38,42 @@ export default function BasicCard(props) {
             Projeto <br />
             {props.task.projectName}
           </Typography>
-          <br />
+        </CardContent>
+        <CardActions>
+          <Button onClick={OpenModal} size="small">Detalhes da tarefa</Button>
+        </CardActions>
+      </Card>
 
+      <Modal
+        open={open}
+        onClose={CloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography color="text.secondary">
+            Tarefa
+          </Typography>
+          <Typography variant="h5" component="div" gutterBottom>
+            {props.task.name}
+          </Typography>
+          <br />
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Descrição <br />
+            {props.task.description}
+          </Typography>
+          <br />
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Projeto <br />
+            {props.task.projectName}
+          </Typography>
+          <br />
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             Colaborador <br />
             {props.task.collaboratorName}
-          </Typography> 
-        </CardContent>
-        <CardActions>
-          <Button size="small">Detalhes da tarefa</Button>
-        </CardActions>
-      </Card>
+          </Typography>
+        </Box>
+      </Modal>
     </Grid>
   );
 }
